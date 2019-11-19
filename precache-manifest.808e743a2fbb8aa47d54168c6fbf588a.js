84f7ba3baaaaa6e509f60f6761b4e3a42c8f9860
Tyson Rai <jovensrai@gmail.com>
2019-11-18 16:45:03 -0800

    Updates

---
+++ precache-manifest.808e743a2fbb8aa47d54168c6fbf588a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb28b503ce8cdfde284452119036e9f",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "e38758eb1431ffe61ccc",
+    "url": "/weatherapp/static/css/main.0625e77c.chunk.css"
+  },
+  {
+    "revision": "617e3d962e14963bc794",
+    "url": "/weatherapp/static/js/2.e51c2dd4.chunk.js"
+  },
+  {
+    "revision": "e38758eb1431ffe61ccc",
+    "url": "/weatherapp/static/js/main.3ab33af1.chunk.js"
+  },
+  {
+    "revision": "442eeed50fccb8309b4f",
+    "url": "/weatherapp/static/js/runtime-main.bd627ee1.js"
+  }
+]);
